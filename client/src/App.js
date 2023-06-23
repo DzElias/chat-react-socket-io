306d535b43ada980572a60a386754173daaf5f3e
Elias Diaz <deelias150lost@gmail.com>
2023-06-23 18:03:05 -0400

    uri sockets removed

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState, useEffect } from "react";
 
-const socket = io("http://localhost:4000/");
+const socket = io();
 
 function App() {
   const [message, setMessage] = useState("");
@@ -27,7 +27,7 @@ function App() {
     <div className="h-screen bg-zinc-800 text-white flez items-center justify-center">
       <header className="App-header">
         <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
-        <h1 className="text-2xl font-bold my-2">Chat React</h1>
+          <h1 className="text-2xl font-bold my-2">Chat React</h1>
           <input
             type="text"
             onChange={(e) => setMessage(e.target.value)}
@@ -35,15 +35,17 @@ function App() {
             className="border-2 border-zinc-500 p-2 text-black w-full"
           />
 
-          
-          <ul className='h-80 overflow-y-auto'>
-          {messages.map((message, index) => (
-            <li key={index} className='my-2 p-2 text-sm rounded-md bg-sky-700 ml-auto'>
-              <p>
-                {message.from}: {message.body}
-              </p>
-            </li>
-          ))}
+          <ul className="h-80 overflow-y-auto">
+            {messages.map((message, index) => (
+              <li
+                key={index}
+                className="my-2 p-2 text-sm rounded-md bg-sky-700 ml-auto"
+              >
+                <p>
+                  {message.from}: {message.body}
+                </p>
+              </li>
+            ))}
           </ul>
         </form>
       </header>
